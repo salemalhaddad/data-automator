a27f78a9c11979d2923827591bdb463e63acf28f
Salem Al Haddad <salem.alhaddad@hotmail.com>
2023-07-10 17:49:25 +0400

    redefined tree again

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('upload.html')
+    return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
