9942c57745723a67c5a85b86d752255756850e25
Salem Al Haddad <salem.alhaddad@hotmail.com>
2023-07-12 10:43:41 +0400

    deployed form

--- app.py
+++ app.py
@@ -96,5 +96,5 @@ def upload_file():
 	return "File uploaded and processed successfully"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=2343)
 
